97d6a84ca1b40a734b16705a2eab2b79dae7d9eb
Samuel El Nunes <samuthekid@gmail.com>
2019-01-03 02:19:33 +0000

    first stable version

--- index.js
+++ index.js
@@ -1,42 +1,69 @@
-const colors = ['2274a5', 'f75c03', 'f1c40f', 'd90368', '00cc66']
+let colorsCnt = 0
+const colorsTotal = 12
+const colorsCSS = `ffelem {
+  color: #F6F6F6;
+  border-radius: 1.5px;
+}
+.color0 { background-color: #1EA896 }
+.color1 { background-color: #FF6164 }
+.color2 { background-color: #0071BC }
+.color3 { background-color: #1FB85C }
+.color4 { background-color: #F75C03 }
+.color5 { background-color: #659B5E }
+.color6 { background-color: #FAA916 }
+.color7 { background-color: #C2202C }
+.color8 { background-color: #B52890 }
+.color9 { background-color: #4930F0 }
+.color10 { background-color: #5A136C }
+.color11 { background-color: #17442B }`
+
 const selections = []
 
 const initFF = () => {
 
   const onKeyDown = e => {
-    // console.log('key down ', e.key)
-    if (e.target.nodeName === 'INPUT') return false
+    if (e.target.nodeName === 'INPUT' || event.metaKey) return false
 
     if (e.key === 'f') {
-      const text = window.getSelection().toString()
+      const text = window.getSelection().toString().trim()
       if (text) {
-        data = { text: togglBR(text, true), tag: `<b class="ff">${text}</b>` }
-        replaceOnDOM(data.text, data.tag)
-        selections.push(data)
-      } else
-        removeLastFromDOM()
+        if (selections.includes(text))
+          removeFromDOM(text)
+        else {
+          findAndReplaceDOMText(document.body,
+            {
+              preset: 'prose',
+              find: text,
+              wrap: 'ffelem',
+              wrapClass: `color${colorsCnt++%colorsTotal} ${text}`,
+            })
+          window.getSelection().empty()
+          selections.push(text)
+        }
+      } else {
+        selections.length && removeFromDOM(selections[selections.length - 1])
+      }
     } else if (e.key === 'd')
       while(selections.length)
-        removeLastFromDOM()
+        removeFromDOM(selections[selections.length - 1])
   }
 
-  const style = document.createElement("style")
-  style.innerHTML = ".ff {background-color: lightblue}"
+  const style = document.createElement('style')
+  style.innerHTML = colorsCSS
   document.head.appendChild(style)
 
   document.body.addEventListener('keydown', onKeyDown)
 }
 
-const removeLastFromDOM = () => {
-  data = selections.pop()
-  data && replaceOnDOM(data.tag, togglBR(data.text, false))
+const removeFromDOM = (text) => {
+  if (!text) return false
+  selections.splice(selections.indexOf(text), 1)
+  var elems = Array.prototype.slice.call( document.getElementsByTagName('ffelem') )
+  elems.forEach(e => {
+    const tagText = e.className.substring(e.className.indexOf(' ') + 1)
+    if(tagText === text)
+      e.replaceWith(e.innerHTML)
+  })
 }
-
-const togglBR = (text, x) => x
-  ? text.replace(new RegExp('\n', 'g'), '<br>\n')
-  : text.replace(new RegExp('<br>\n', 'g'), '\n')
-
-const replaceOnDOM = (text, val) =>
-  document.body.innerHTML = document.body.innerHTML.replace(new RegExp(text, 'g'), val)
 
 window.onload = initFF()
