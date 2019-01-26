4b4f4bc8cdf51e006c4131564c1a2d6d7925be59
Daniel Leal <danielgek@gmail.com>
2019-01-26 02:24:53 +0000

    big refactor: actually leverage the api of search lib

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let colorsCnt = 0
-const colorsTotal = 12
-const colorsCSS = `ffelem {
+let colorsCnt = 0;
+const colorsTotal = 12;
+const colorsCSS = `
+.ffelem {
   color: #F6F6F6;
-  border-radius: 1.5px;
+  position: relative;
+  z-index: 0;
+  display: inline-block;
+}
+.ffelem::before {
+  content: '';
+  position: absolute;
+  top: -1px;
+  left: -2px;
+  right: -2px;
+  bottom: -1px;
+  background: inherit;
+  z-index: -1;
+  border-radius: 2px;
+  transition: transform .3s;
+}
+.active::before {
+  transform: scale(1.1);
 }
 .color0 { background-color: #1EA896 }
 .color1 { background-color: #FF6164 }
@@ -15,55 +33,83 @@ const colorsCSS = `ffelem {
 .color8 { background-color: #B52890 }
 .color9 { background-color: #4930F0 }
 .color10 { background-color: #5A136C }
-.color11 { background-color: #17442B }`
+.color11 { background-color: #17442B }`;
 
-const selections = []
+let selections = [];
 
 const initFF = () => {
-
   const onKeyDown = e => {
-    if (e.target.nodeName === 'INPUT' || event.metaKey) return false
+    if (e.target.nodeName === "INPUT" || event.metaKey) return false;
 
-    if (e.key === 'f') {
-      const text = window.getSelection().toString().trim()
+    if (e.key === "f") {
+      const text = window
+        .getSelection()
+        .toString()
+        .trim();
       if (text) {
-        if (selections.includes(text))
-          removeFromDOM(text)
-        else {
-          findAndReplaceDOMText(document.body,
-            {
-              preset: 'prose',
-              find: text,
-              wrap: 'ffelem',
-              wrapClass: `color${colorsCnt++%colorsTotal} ${text}`,
-            })
-          window.getSelection().empty()
-          selections.push(text)
+        const finder = selections.find(
+          selection => selection.finder.options.find === text
+        );
+        if (finder) {
+          selections = selections.filter(selection => {
+            if (selection.finder.options.find !== text) return true;
+            selection.finder.revert();
+            return false;
+          });
+        } else {
+          const color = `color${colorsCnt++ % colorsTotal}`;
+          const currentElements = [];
+          const finder = findAndReplaceDOMText(document.body, {
+            preset: "prose",
+            find: text,
+            replace: function(portion, match) {
+              const div = document.createElement("div");
+              div.classList.add("ffelem", color);
+              div.innerHTML = portion.text;
+              currentElements.push(div);
+              return div;
+            }
+          });
+          debugger;
+          window.getSelection().empty();
+          const currentSelection = { finder, elements: currentElements };
+          currentElements.forEach(element => {
+            element.onmouseover = onHover(currentSelection);
+            element.onmouseout = onHover(currentSelection);
+          });
+          selections.push(currentSelection);
         }
       } else {
-        selections.length && removeFromDOM(selections[selections.length - 1])
+        if (selections.length) {
+          selections.pop().finder.revert();
+        }
       }
-    } else if (e.key === 'd')
-      while(selections.length)
-        removeFromDOM(selections[selections.length - 1])
-  }
+    } else if (e.key === "d") {
+      selections.forEach(selection => selection.finder.revert());
+      selections = [];
+    }
+  };
 
-  const style = document.createElement('style')
-  style.innerHTML = colorsCSS
-  document.head.appendChild(style)
+  const style = document.createElement("style");
+  style.innerHTML = colorsCSS;
+  document.head.appendChild(style);
 
-  document.body.addEventListener('keydown', onKeyDown)
-}
+  document.body.addEventListener("keydown", onKeyDown);
+};
 
-const removeFromDOM = (text) => {
-  if (!text) return false
-  selections.splice(selections.indexOf(text), 1)
-  var elems = Array.prototype.slice.call( document.getElementsByTagName('ffelem') )
-  elems.forEach(e => {
-    const tagText = e.className.substring(e.className.indexOf(' ') + 1)
-    if(tagText === text)
-      e.replaceWith(e.innerHTML)
-  })
-}
+const onHover = currentSelection => {
+  return event => {
+    if (event.type === "mouseover") {
+      currentSelection.elements.forEach(element =>
+        element.classList.add("active")
+      );
+    } else {
+      currentSelection.elements.forEach(element =>
+        element.classList.remove("active")
+      );
+    }
+    console.log(event, currentSelection);
+  };
+};
 
-window.onload = initFF()
+window.onload = initFF();
